1316dd342636cc8c75e15870ac68e9583e890385
Trevor Jay <trevorjay@gmail.com>
2017-08-04 04:41:00 +0000

    testing floodfill

--- Playground/FloodFillTest/Source/Test.hx
+++ Playground/FloodFillTest/Source/Test.hx
@@ -1,10 +1,27 @@
 package;
 
 import starling.display.Sprite;
+import starling.display.Sprite;
+import starling.textures.Texture;
+import starling.display.Image;
+
+import openfl.Lib.getTimer;
+import openfl.display.BitmapData;
 
 class Test extends Sprite {
   public function new() {
     super();
-    trace('here');
+
+    var bmp = new BitmapData(800,600);
+    var then = getTimer();
+    bmp.floodFill(400,300,0xFF00FF00);
+
+    trace(getTimer() - then);
+
+    var tex = Texture.fromBitmapData(bmp);
+    var img = new Image(tex);
+
+    addChild(img);
+
   }
 }
