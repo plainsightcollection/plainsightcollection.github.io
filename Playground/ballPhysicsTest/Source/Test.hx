38e4fdbb78c145ce8541b30e909270553f16f24a
Trevor Jay <trevorjay@gmail.com>
2017-08-04 03:56:36 +0000

    calculating incidence angle with math3D

--- Playground/ballPhysicsTest/Source/Test.hx
+++ Playground/ballPhysicsTest/Source/Test.hx
@@ -6,18 +6,58 @@ import starling.textures.Texture;
 import starling.display.Image;
 
 import openfl.geom.Vector3D;
+import openfl.geom.Matrix3D;
 import openfl.display.BitmapData;
 
 class Test extends Sprite {
   public function new() {
     super();
 
-    var cnv = new openfl.display.Sprite();
-    cnv.graphics.lineStyle(5,0x00FF00);
-    cnv.graphics.moveTo(0,0);
-    cnv.graphics.lineTo(640,480);
+    var xo = 320;
+    var yo = 480;
 
-    var bmp = new BitmapData(640,480,true,0x00000000);
+    var cnv = new openfl.display.Sprite();
+
+    var normal = new Vector3D(0,1,0,0);
+
+    var trans = new Matrix3D();
+    var v = normal;
+
+    trans.identity();
+    trans.appendScale(1,240,1);
+    v = trans.transformVector(v);
+    cnv.graphics.lineStyle(5,0x000000);
+    cnv.graphics.moveTo(xo,yo);
+    cnv.graphics.lineTo(xo+v.x,yo-v.y);
+
+    v = normal;
+    trans.identity();
+    trans.appendScale(1,240,1);
+    trans.appendRotation(45,new Vector3D(0,0,1,0));
+    v = trans.transformVector(v);
+    cnv.graphics.lineStyle(5,0x0000FF);
+    cnv.graphics.moveTo(xo,yo);
+    cnv.graphics.lineTo(xo+v.x,yo-v.y);
+
+    v = normal;
+    trans.identity();
+    trans.appendRotation(45,new Vector3D(0,0,1,0));
+    v = trans.transformVector(v);
+    var i = v.dotProduct(normal);
+    i = Math.acos(i);
+    i = (i*180)/Math.PI;
+    i = i*-1; //reverse
+
+    v = normal;
+    trans.identity();
+    trans.appendScale(1,240,1);
+    trans.appendRotation(i,new Vector3D(0,0,1,0));
+    v = trans.transformVector(v);
+    cnv.graphics.lineStyle(5,0xFF0000);
+    cnv.graphics.moveTo(xo,yo);
+    cnv.graphics.lineTo(xo+v.x,yo-v.y);
+
+    var bmp = new BitmapData(640,480,true,0xFFFFFFFF);
     bmp.draw(cnv);
 
     var tex = Texture.fromBitmapData(bmp);
