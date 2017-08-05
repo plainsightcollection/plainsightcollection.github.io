547d5d50df81b921f85970ed0d2db21867e9c219
Trevor Jay <trevorjay@gmail.com>
2017-08-05 00:47:04 +0000

    HitTest skeleton

---
+++ Playground/HitTest/Source/HitTest.hx
@@ -0,0 +1,18 @@
+package;
+
+import openfl.display.Sprite;
+import starling.core.Starling;
+
+class Startup extends Sprite {
+  private var starling:Starling;
+  
+  
+  public function new () {
+    super ();
+    
+    starling = new Starling (Test, stage);
+    starling.showStats = true;
+    starling.start ();
+  }
+  
+}
---
+++ Playground/HitTest/Source/Text.hs
@@ -0,0 +1,10 @@
+package;
+
+import starling.display.Starling;
+
+class Test extends Sprite {
+  public function new() {
+    super();
+    trace("here");
+  }
+}
