5ec6f46a256a643f24494dabdac3fcc6adb0dd7a
Trevor Jay <trevorjay@gmail.com>
2017-08-06 01:02:50 +0000

    should compile

---
+++ Playground/BounceTest/Source/Main.hx
@@ -0,0 +1,17 @@
+package;
+
+import openfl.display.Sprite;
+import starling.core.Starling;
+
+class Main extends Sprite {
+  private var starling:Starling;
+  
+  public function new() {
+    super();
+    
+    starling = new Starling(Test,stage);
+    starling.showStats = true;
+    starling.start();
+  }
+  
+}
