24add95fbe27fa006ab90f79ccfbc35804c8c4fa
Trevor Jay <trevorjay@gmail.com>
2017-08-10 04:52:01 +0000

    placeholder tiles

--- games/Flippy/Source/Flippy.hx
+++ games/Flippy/Source/Flippy.hx
@@ -2,6 +2,10 @@ package;
 
 import starling.display.Sprite;
 import starling.display.Quad;
+import starling.textures.Texture;
+import starling.display.Image;
+
+import openfl.display.BitmapData;
 
 class Flippy extends Sprite {
   private static inline var MT = 11;
@@ -12,6 +16,12 @@ class Flippy extends Sprite {
 
   private var hLinks:Array<Quad>;
   private var vLinks:Array<Quad>;
+
+  private var dragonTex:Texture;
+  private var skullTex:Texture;
+
+  private var dragons:Array<Image>;
+  private var skulls:Array<Image>;
 
   public function new() {
     super();
@@ -59,6 +69,51 @@ class Flippy extends Sprite {
         q.y = MT+j*SIZE+(SIZE/2);
         vLinks.push(q);
         addChild(q);
+      }
+    }
+
+    var bmp:BitmapData;
+    var cnv:openfl.display.Sprite;
+    var trans = new openfl.geom.Matrix();
+    trans.translate(SIZE/18,SIZE/18);
+
+    bmp = new BitmapData(SIZE,SIZE,true,0);
+    cnv = new openfl.display.Sprite();
+    cnv.graphics.beginFill(0xFFFF0000);
+    cnv.graphics.drawCircle(SIZE*4/9,SIZE*4/9,SIZE*4/9);
+    cnv.graphics.endFill();
+    bmp.draw(cnv,trans);
+
+    dragonTex = Texture.fromBitmapData(bmp);
+    bmp.dispose();
+
+    bmp = new BitmapData(SIZE,SIZE,true,0);
+    cnv = new openfl.display.Sprite();
+    cnv.graphics.beginFill(0xFF404040);
+    cnv.graphics.drawCircle(SIZE*4/9,SIZE*4/9,SIZE*4/9);
+    cnv.graphics.endFill();
+    bmp.draw(cnv,trans);
+
+    skullTex = Texture.fromBitmapData(bmp);
+    bmp.dispose();
+
+    var img:Image;
+
+    dragons = new Array();
+    skulls = new Array();
+    for (j in 0...5) {
+      for (i in 0...8) {
+        img = new Image(skullTex);
+        img.x = ML+i*SIZE;
+        img.y = MT+j*SIZE;
+        skulls.push(img);
+        addChild(img);
+
+        img = new Image(dragonTex);
+        img.x = ML+i*SIZE;
+        img.y = MT+j*SIZE;
+        dragons.push(img);
+        addChild(img);
       }
     }
 
