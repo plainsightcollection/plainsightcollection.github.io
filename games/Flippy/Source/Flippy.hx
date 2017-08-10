636ef530e73539a87d8c45d9f93a1327d55487c0
Trevor Jay <trevorjay@gmail.com>
2017-08-10 03:29:00 +0000

    putting in quads

--- games/Flippy/Source/Flippy.hx
+++ games/Flippy/Source/Flippy.hx
@@ -8,6 +8,10 @@ class Flippy extends Sprite {
   private static inline var ML = 11;
   private static inline var SIZE = 72;
   private static inline var LINK = 14;
+  private static inline var CLR = 0xFF44D62C;
+
+  private var hLinks:Array<Quad>;
+  private var vLinks:Array<Quad>;
 
   public function new() {
     super();
@@ -34,7 +38,30 @@ class Flippy extends Sprite {
     reset.y = 378;
     addChild(reset);
 
-    trace("here");
+    var q:Quad;
+
+    hLinks = new Array();
+    for (j in 0...5) {
+      for (i in 0...7) {
+        q = new Quad(SIZE,LINK,CLR);
+        q.x = ML+i*SIZE+(SIZE/2);
+        q.y = MT+j*SIZE+(SIZE-LINK)/2;
+        hLinks.push(q);
+        addChild(q);
+      }
+    }
+
+    vLinks = new Array();
+    for (j in 0...4) {
+      for (i in 0...8) {
+        q = new Quad(LINK,SIZE,CLR);
+        q.x = ML+i*SIZE+(SIZE-LINK)/2;
+        q.y = MT+j*SIZE+(SIZE/2);
+        vLinks.push(q);
+        addChild(q);
+      }
+    }
+
   }
 
 }
