37cfa78080c0c80f9bd7454006a938b9dc7d78f8
Trevor Jay <trevorjay@gmail.com>
2017-08-11 10:10:27 +0000

    dx

--- web/bookmark.js
+++
@@ -1,1 +0,0 @@
-t=document.title;document.title="Loading...";r=new XMLHttpRequest();r.onload=function(e){eval(e.currentTarget.responseText)};r.open("GET","https://plainsightcollection.github.io/web/loader.js",true);r.send();undefined;
