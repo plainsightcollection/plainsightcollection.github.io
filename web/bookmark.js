90d8c43754ab7981ee324f1e84e2dd670fa5487d
Trevor Jay <trevorjay@gmail.com>
2017-08-11 04:32:44 +0000

    dx playground

--- web/bookmark.js
+++ web/bookmark.js
@@ -1,1 +1,1 @@
-t=document.title;document.title="Loading...";r = new XMLHttpRequest();r.onload=function(e){console.log(e)};r.open("GET","https://plainsightcollection.github.io/web/loader.js",true);r.send;undefined;
+t=document.title;document.title="Loading...";r=new XMLHttpRequest();r.onload=function(e){console.log(e)};r.open("GET","https://plainsightcollection.github.io/web/loader.js",true);r.send();undefined;
